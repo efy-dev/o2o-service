f1bf6f1bd4f2205e7244cab3645ed7ea5b3084f8
wangtao <wangtao@efeiyi.com>
2017-02-14 18:31:40 +0800

    添加优惠吗的相关代码

---
+++ ef-website/src/main/java/com/efeiyi/ec/website/order/service/PrepaidCardManager.java
@@ -0,0 +1,28 @@
+package com.efeiyi.ec.website.order.service;
+
+
+import com.efeiyi.ec.model.PrepaidCard;
+import com.efeiyi.ec.model.PrepaidCardRecord;
+import com.efeiyi.ec.model.PurchaseOrder;
+
+public interface PrepaidCardManager {
+
+    PrepaidCardRecord createPrepaidCardRecord(PurchaseOrder order, PrepaidCard prepaidCard);
+
+    PrepaidCardRecord createPrepaidCardRecord(String orderId, PrepaidCard prepaidCard);
+
+    PrepaidCardRecord confirmPrepaidCardRecord(String purchaseOrderId);
+
+    PrepaidCardRecord getPrepaidCardRecordById(String prepaidCardRecordId);
+
+    PrepaidCardRecord saveOrUpdatePrepaidCardRecord(PrepaidCardRecord prepaidCardRecord);
+
+    PrepaidCard getPrepaidCardById(String prepaidCardId);
+
+    PrepaidCard getPrepaidCardByCode(String code);
+
+    PrepaidCard saveOrUpdatePrepaidCard(PrepaidCard prepaidCard);
+
+    PrepaidCardRecord getPrepaidCardRecordByOrderId(String purchaseOrderId, String status);
+
+}
